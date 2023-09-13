5678c72162a2be11af687fcd01bcebe749250bf4
skrnagrh <suk12on512@gmail.com>
2023-09-13 08:28:29 +0700

    perubahan navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,10 +6,12 @@
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 my-2 my-md-0" id="sidebarToggle" href="#!"><i
             class="fas fa-bars"></i></button>
 
-    <ul class="navbar-nav d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+    <ul class="navbar-nav d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-capitalize" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">{{ Auth()->user()->name }} <i class="fas fa-user fa-fw"></i></a>
+            <a class="nav-link dropdown-toggle text-capitalize" id="navbarDropdown" href="#" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false"><span
+                    class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">{{
+                    Auth()->user()->name }}</span> <i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 <li><a class="dropdown-item" href="/">Home Page</a></li>
                 <li>
